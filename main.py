43ec14fa036ae6fea88b49b6cf2603c11511a2b8
Karelito00 <karelkdv2000@gmail.com>
2021-08-08 23:10:34 -0400

    Add I/O to main

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from parse import PrepareData
 from statistics import Summary
 
-data = PrepareData('result.json', 'Telegram').run()
+Platforms = ['Whatsapp', 'Telegram']
+def show_platforms():
+    for i in range(len(Platforms)):
+        print("[" + str(i + 1) + "] " + Platforms[i])
+
+print("Name of the file:")
+filename = input()
+
+print("Platform:")
+show_platforms()
+selected_platform = int(input())
+
+
+data = PrepareData(filename, Platforms[selected_platform - 1]).run()
 summary = Summary(data)
 
 summary.run()
